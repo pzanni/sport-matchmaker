817aef6a595fea8d22c4763b0a9d8d387bc652c1
Anton <anton.moroz@helsinki.fi>
2018-03-29 17:54:28 +0300

    usersreduceria testattu, lisää testitapauksia tulee, kun koodataan lisää actioneja mukaan

---
+++ src/tests/testData.js
@@ -0,0 +1,5 @@
+const firstUser = { username: 'Anton', email: 'anton@anton.com' }
+const secondUser = { username: 'Anni', email: 'anni@anni.com' }
+const users = [firstUser, secondUser]
+
+export { users }
--- src/tests/usersActions.test.js
+++ src/tests/usersActions.test.js
@@ -1,10 +1,7 @@
 import { setUsers } from '../reducers/users'
+import { users } from './testData'
 
 it('should create an action of type SET_USERS for altering user state from given array of users', () => {
-  const firstUser = { username: 'Anton', email: 'anton@anton.com' }
-  const secondUser = { username: 'Anni', email: 'anni@anni.com' }
-  const users = [firstUser, secondUser]
-
   const action = setUsers(users)
 
   //toBe ei toimi objekteille, tarvitaan toEqual jokaisen keyn vertailua varten
