919db5089b411a93a69edb11740b61a17247b259
Anton <anton.moroz@helsinki.fi>
2018-04-16 14:29:04 +0300

    e2e - testing beginning with cucumber and puppeteer. Added feature + step have no relation to this project but can work as a beginning reference

---
+++ src/tests/features/steps/Rockingrackets.steps.test.js
@@ -0,0 +1,25 @@
+import { defineFeature, loadFeature } from 'jest-cucumber'
+import puppeteer from 'puppeteer'
+
+const feature = loadFeature('./src/tests/features/Rockingrackets.feature')
+
+defineFeature(feature, (test) => {
+  let browser
+  let page
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({ headless: false })
+    page = await browser.newPage()
+  })
+
+  test('Landing on default page', async ({ given, when, then }) => {
+    given('I go to rockingrackets website', async () => {
+      await page.goto('http://rockingrackets.com/')
+    })
+
+    then('It has a title', async () => {
+      const title = await page.title()
+      expect(title).toBeTruthy()
+    })
+  })
+})
