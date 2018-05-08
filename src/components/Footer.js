5eea01dc68f97a99def27ae3ce092e1e47d8749a
Anni Puurunen <anni.puurunen@cloudstreet.co>
2018-05-08 17:56:50 +0300

    Fix footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BottomNavigation, Typography } from 'material-ui';
+import { Column, Row } from 'simple-flexbox'
 
-// IMPROVE THIS PLZ
 const Footer = (props) => {
-  return (<div>Made by Anton & Anni (make it stick to the bottom somehow...)</div>)
+
+  const styles = {
+    position: 'relative',
+    left: 0,
+    bottom: 0,
+    right: 0
+  }
+
+  return (
+    <BottomNavigation style={styles}>
+      <Row vertical="center" horizontal="center">
+        <Typography variant="caption">
+          Made by Anton & Anni
+        </Typography>
+      </Row>
+    </BottomNavigation>
+  )
 }
 
 export default Footer
