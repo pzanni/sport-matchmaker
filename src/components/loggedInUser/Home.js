d90c4a1cc7792bde80166c8e5fed5e3b30a2d0ea
Anni Puurunen <anni.puurunen@cloudstreet.co>
2018-05-13 18:58:45 +0300

    what to do with home page?

--- src/components/loggedInUser/Home.js
+++ src/components/loggedInUser/Home.js
@@ -1,17 +1,41 @@
 import React from "react";
 import { ConnectedChallengeList, ConnectedAcceptedChallengesList } from './Challenge'
 import VisibilityFilter from './VisibilityFilter'
+import { Column, Row } from 'simple-flexbox'
+import { Paper, Typography } from 'material-ui';
 
 const Home = (props) => {
   return (
     <div className="homeRoot">
-      <p>Kirjautuneen käyttäjän etusivu</p>
-      <p>Alla on lista kaikista käyttäjän olemassaolevista haasteista</p>
-      <VisibilityFilter />
-      <ConnectedChallengeList />
-      <p>Alla on lista kaikista käyttäjän hyväksytyistä haasteista</p>
-      <ConnectedAcceptedChallengesList />
+      <MyChallenges />
     </div>
+  )
+}
+
+const MyChallenges = () => {
+  return (
+    <Row style={{ margin: '25px' }} vertical="start">
+      <Column flexGrow={0.5} style={{ margin: '10px' }}>
+        <Paper style={{ padding: '25px' }}>
+          <Typography variant="display1">
+            My challenges
+          </Typography>
+          <p>Alla on lista kaikista käyttäjän olemassaolevista haasteista</p>
+          <VisibilityFilter />
+          <ConnectedChallengeList />
+        </Paper>
+      </Column>
+
+      <Column flexGrow={0.5} style={{ margin: '10px' }}>
+        <Paper style={{ padding: '25px' }}>
+          <Typography variant="display1">
+            Accepted challenges
+          </Typography>
+          <p>Alla on lista kaikista käyttäjän hyväksytyistä haasteista</p>
+          <ConnectedAcceptedChallengesList />
+        </Paper>
+      </Column>
+    </Row>
   )
 }
 
