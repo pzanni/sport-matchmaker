e7cd91e5b10fd4a6ad6a288608f26ae859e7215d
Anton Moroz <anton.moroz@helsinki.fi>
2018-05-27 22:27:57 +0300

    Remove unused route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,6 +2,5 @@ export const SIGN_UP = "/signup";
 export const SIGN_IN = "/signin";
 export const LANDING = "/";
 export const HOME = "/home";
-export const ACCOUNT = "/account";
 export const USERS = "/users";
 export const USERSwID = "/users/:id";
