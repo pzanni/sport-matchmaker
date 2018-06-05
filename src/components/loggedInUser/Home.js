0ef52567c6f8e47fe485c248fe6dd3250b6016eb
Anton <anton.moroz@helsinki.fi>
2018-06-05 21:02:59 +0300

    Home pagestyle and component refactor - moved to tabular style and less DRY. Up next buddy system and changing challenge style from tables to lists for more stylistic freedom

--- src/components/loggedInUser/Home.js
+++ src/components/loggedInUser/Home.js
@@ -1,54 +1,118 @@
 import React from "react";
 import { connect } from 'react-redux'
 import { Column, Row } from 'simple-flexbox'
-import { Paper, Typography } from 'material-ui';
+import { Paper, Typography, Tab, Tabs } from 'material-ui';
 
 import { ConnectedList } from './Challenge'
 import ChallengeVisibilityFilter from './VisibilityFilter'
 import { pendingFilterChange, acceptedFilterChange } from '../../reducers/filters'
 
+const styles = {
+  borderedDiv: {
+    backgroundColor: 'white',
+    border: '0',
+    borderRadius: '5px',
+    boxShadow: '1px 1px 3px 0',
+    marginTop: '20px',
+    minHeight: '400px',
+    padding: '20px'
+  }
+}
+
 const Home = (props) => {
   return (
     <div className="homeRoot">
-      <ConnectedMyChallenges />
+      <ChallengeTabs />
     </div>
   )
 }
 
-const MyChallenges = (props) => {
-  const { filter, acceptedFilterChange, pendingFilterChange } = props
-  return (
-    <Row style={{ margin: '25px' }} vertical="start">
-      <Column flexGrow={0.5} style={{ margin: '10px' }}>
-        <Paper style={{ padding: '40px', margin: '20px', minHeight: '500px' }}>
-          <Typography variant="display1">
-            My challenges
-          </Typography>
-          <ChallengeVisibilityFilter
-            filter={filter.pendingChallenges}
-            dispatchableFunction={pendingFilterChange}
-            firstOption={'All'}
-            secondOption={'Sent'}
-            thirdOption={'Received'} />
-          <ConnectedList condition={false} propFilter={filter.pendingChallenges} />
-        </Paper>
-      </Column>
+class ChallengeTabs extends React.Component {
+  state = {
+    value: 0
+  }
 
-      <Column flexGrow={0.5} style={{ margin: '10px' }}>
-        <Paper style={{ padding: '40px', margin: '20px', minHeight: '500px' }}>
-          <Typography variant="display1">
-            Accepted challenges
-          </Typography>
-          <ChallengeVisibilityFilter
-            filter={filter.acceptedChallenges}
-            dispatchableFunction={acceptedFilterChange}
-            firstOption={'All'}
-            secondOption={'Accepted'}
-            thirdOption={'Completed'} />
-          <ConnectedList condition={true} propFilter={filter.acceptedChallenges} />
-        </Paper>
-      </Column>
-    </Row>
+  handleChange = (event, value) => {
+    this.setState({ value });
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <Tabs value={value} onChange={this.handleChange}>
+          <Tab label="Pending challenges" />
+          <Tab label="Ongoing challenges" />
+        </Tabs>
+        {value === 0 && <ConnectedPendingChallenges />}
+        {value === 1 && <ConnectedAcceptedChallenges />}
+      </div>
+    )
+  }
+}
+
+const PendingChallenges = (props) => {
+  const { filter, pendingFilterChange } = props
+  return (
+    <ChallengeType
+      filter={filter.pendingChallenges}
+      dispatchableFunction={pendingFilterChange}
+      title='My challenges'
+      firstOption='All'
+      secondOption='Sent'
+      thirdOption='Received'
+      condition={false}
+      // Voisi käyttää filter (1. param) - mutta pidetään tilanne tälläisenä havainnollistuksen vuoksi
+      // alikomponentissa
+      propFilter={filter.pendingChallenges}
+    />
+  )
+}
+
+const AcceptedChallenges = (props) => {
+  const { filter, acceptedFilterChange } = props
+  return (
+    <ChallengeType
+      filter={filter.acceptedChallenges}
+      dispatchableFunction={acceptedFilterChange}
+      title='Accepted challenges'
+      firstOption='All'
+      secondOption='Accepted'
+      thirdOption='Completed'
+      condition={true}
+      propFilter={filter.acceptedChallenges}
+    />
+  )
+}
+
+//Pending challenge and accepted challenge types are built upon this via props
+const ChallengeType = (props) => {
+  const {
+    filter,
+    dispatchableFunction,
+    title,
+    firstOption,
+    secondOption,
+    thirdOption,
+    condition,
+    propFilter
+   } = props
+
+
+  return (
+    <div style={styles.borderedDiv}>
+      <Typography variant="display1">
+        {title}
+      </Typography>
+      <ChallengeVisibilityFilter
+        filter={filter}
+        dispatchableFunction={dispatchableFunction}
+        firstOption={firstOption}
+        secondOption={secondOption}
+        thirdOption={thirdOption} />
+      <ConnectedList condition={condition} propFilter={propFilter} />
+    </div>
   )
 }
 
@@ -65,7 +129,8 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const ConnectedMyChallenges = connect(mapStateToProps, mapDispatchToProps)(MyChallenges)
+const ConnectedPendingChallenges = connect(mapStateToProps, mapDispatchToProps)(PendingChallenges)
+const ConnectedAcceptedChallenges = connect(mapStateToProps, mapDispatchToProps)(AcceptedChallenges)
 
 export default connect(mapStateToProps)(Home)
-export { ConnectedMyChallenges }
+export { ConnectedPendingChallenges, ConnectedAcceptedChallenges }
